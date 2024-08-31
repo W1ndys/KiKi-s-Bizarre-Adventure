ef09dbb42e703a8da18eb6872b989dde98312664
W1ndys <w1ndys@outlook.com>
2024-08-31 20:44:20 +0800

    恢复地板

--- backGround.js
+++ backGround.js
@@ -17,6 +17,7 @@ class Layer{
         context.drawImage(this.image,this.x+this.width,this.y,this.width,this.height)
     }
 }
+
 export class BackGround{
     constructor(game,level){
         this.game=game
@@ -30,10 +31,10 @@ export class BackGround{
         console.log(this.layerImage1,this.layerImage2,this.layerImage3,this.layerImage4,this.layerImage5)
         this.layer1=new Layer(this.game,this.width,this.height,0,this.layerImage1)
         this.layer2=new Layer(this.game,this.width,this.height,0.2,this.layerImage2)
-        // this.layer3=new Layer(this.game,this.width,this.height,0.4,this.layerImage3)
+        this.layer3=new Layer(this.game,this.width,this.height,0.4,this.layerImage3)
         this.layer4=new Layer(this.game,this.width,this.height,0.6,this.layerImage4)
-        // this.layer5=new Layer(this.game,this.width,this.height,0.8,this.layerImage5)
-        this.BackGroundLayers=[this.layer1,this.layer2,this.layer4]
+        this.layer5=new Layer(this.game,this.width,this.height,0.8,this.layerImage5)
+        this.BackGroundLayers=[this.layer1,this.layer2,this.layer3,this.layer4,this.layer5] // 添加所有层
     }
     update(){
         this.BackGroundLayers.forEach(layer=>{
